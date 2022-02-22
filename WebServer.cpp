5b8f7473e452f31ea117821cd7e3c05e98d19fad
rquit <nabilomi1@gmail.com>
2022-02-22 14:56:02 -0500

    initial commit

---
+++ WebServer.cpp
@@ -0,0 +1,7 @@
+#include "WebServer.h"
+
+#include <iostream>
+
+void hello() {
+    std::cout << "Hello, World!" << std::endl;
+}
---
+++ cmake-build-debug/CMakeFiles/WSLWebServer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WSLWebServer.dir/WebServer.cpp.o"
+  "CMakeFiles/WSLWebServer.dir/network/BindingSocket.cpp.o"
+  "CMakeFiles/WSLWebServer.dir/network/SimpleSocket.cpp.o"
+  "libWSLWebServer.a"
+  "libWSLWebServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WSLWebServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
