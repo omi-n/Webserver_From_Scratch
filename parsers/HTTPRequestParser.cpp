3841d6e65e54543e97f84e4d2bf37a015fb2e24d
rquit <nabilomi1@gmail.com>
2022-03-02 16:34:12 -0500

    add skeleton for http request parser

---
+++ parsers/HTTPRequestParser.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Nabil on 3/2/2022.
+//
+
+#include "HTTPRequestParser.h"
---
+++ parsers/HTTPRequestParser.h
@@ -0,0 +1,20 @@
+//
+// Created by Nabil on 3/2/2022.
+//
+
+#ifndef WSLWEBSERVER_HTTPREQUESTPARSER_H
+#define WSLWEBSERVER_HTTPREQUESTPARSER_H
+
+#include "SimpleParser.h"
+#include "Request.h"
+
+namespace Nabil_Omi_WSL {
+
+class HTTPRequestParser : SimpleParser<Request>  {
+
+};
+
+}
+
+
+#endif //WSLWEBSERVER_HTTPREQUESTPARSER_H
